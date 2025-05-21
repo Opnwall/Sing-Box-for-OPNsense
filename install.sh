78be0e21c41820f12d2851c6ca611283c208fa1c
Opnwall <52145756+Opnwall@users.noreply.github.com>
2025-05-21 09:48:38 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e ''
-echo -e "\033[32m========Sing-Box for OPNsense 代理全家桶一键安装脚本=========\033[0m"
+echo -e "\033[32m========Sing-Box for OPNsense一键安装脚本=========\033[0m"
 echo -e ''
 
 # 定义颜色变量
@@ -36,7 +36,7 @@ log() {
 # 创建目录
 log "$YELLOW" "创建目录..."
 sleep 1
-mkdir -p "$CONF_DIR/sing-box/ui" "$CONF_DIR/tun2socks" || log "$RED" "目录创建失败！"
+mkdir -p "$CONF_DIR/sing-box" || log "$RED" "目录创建失败！"
 
 # 复制文件
 log "$YELLOW" "复制文件..."
@@ -57,10 +57,9 @@ cp -f www/* "$WWW_DIR/" || log "$RED" "www 文件复制失败！"
 cp -f plugins/* "$PLUGINS/" || log "$RED" "plugins 文件复制失败！"
 cp -f actions/* "$ACTIONS/" || log "$RED" "actions 文件复制失败！"
 cp -R -f menu/* "$MODELS_DIR/" || log "$RED" "menu 文件复制失败！"
-cp -R -f ui/* "$CONF_DIR/sing-box/ui/" || log "$RED" "ui 文件复制失败！"
+# cp -R -f ui/* "$CONF_DIR/sing-box/ui/" || log "$RED" "ui 文件复制失败！"
 cp rc.d/* "$RC_DIR/" || log "$RED" "rc.d 文件复制失败！"
 cp conf/config_sing-box.json "$CONF_DIR/sing-box/config.json" || log "$RED" "sing-box 配置文件复制失败！"
-cp conf/config_tun2socks.yaml "$CONF_DIR/tun2socks/config.yaml" || log "$RED" "tun2socks 配置文件复制失败！"
 sleep 1
 
 # 添加服务启动项
@@ -69,8 +68,8 @@ cp -f rc.conf/* "$RC_CONF/" || log "$RED" "rc.conf 文件复制失败！"
 sleep 1
 
 # 启动Tun接口
-log "$YELLOW" "启动tun2socks..."
-service tun2socks start > /dev/null 2>&1
+log "$YELLOW" "启动sing-box..."
+service singbox start > /dev/null 2>&1
 echo ""
 
 # 备份配置文件
@@ -81,32 +80,64 @@ cp "$CONFIG_FILE" "$BACKUP_FILE" || {
 }
 
 # 添加tun接口
-log "$YELLOW" "添加tun接口..."
+log "$YELLOW" "添加 tun_3000 接口..."
 sleep 1
 if grep -q "<if>tun_3000</if>" "$CONFIG_FILE"; then
   echo "存在同名接口，忽略"
-  echo ""
 else
   awk '
   BEGIN { inserted = 0 }
-  /<interfaces>/ { print; next }
-  /<\/interfaces>/ && inserted == 0 {
-    print "    <tun_3000>"
-    print "      <if>tun_3000</if>"
-    print "      <descr>TUN</descr>"
-    print "      <enable>1</enable>"
-    print "      <spoofmac/>"
-    print "      <gateway_interface>1</gateway_interface>"
-    print "      <ipaddr>10.10.0.1</ipaddr>"
-    print "      <subnet>32</subnet>"
-    print "    </tun_3000>"
+  {
+    print
+    if ($0 ~ /<\/lo0>/ && inserted == 0) {
+      print "    <opt10>"
+      print "      <enable>1</enable>"
+      print "      <lock>1</lock>"
+      print "      <descr>TUN</descr>"
+      print "      <if>tun_3000</if>"
+      print "      <ipaddr>172.19.0.2</ipaddr>"
+      print "      <subnet>30</subnet>"
+      print "      <gateway>TUN_GW</gateway>"
+      print "    </opt10>"
+      inserted = 1
+    }
+  }
+  ' "$CONFIG_FILE" > "$TMP_FILE" && mv "$TMP_FILE" "$CONFIG_FILE"
+  echo "接口添加完成"
+fi
+echo ""
+
+# 添加tun网关
+log "$YELLOW" "添加 TUN_GW 网关..."
+sleep 1
+if grep -q "<name>TUN_GW</name>" "$CONFIG_FILE"; then
+  echo "存在同名网关，忽略"
+else
+  awk '
+  BEGIN { inserted = 0 }
+  /<Gateways>/ {
+    print
+    next
+  }
+  /<\/Gateways>/ && inserted == 0 {
+    print "      <gateway_item uuid=\"6639b95f-ee46-423c-b9ab-f042653cd41b\">"
+    print "        <interface>opt10</interface>"
+    print "        <gateway>172.19.0.1</gateway>"
+    print "        <name>TUN_GW</name>"
+    print "        <ipprotocol>inet</ipprotocol>"
+    print "        <descr></descr>"
+    print "        <defaultgw>0</defaultgw>"
+    print "        <monitor_disable>1</monitor_disable>"
+    print "        <disabled>0</disabled>"
+    print "      </gateway_item>"
     inserted = 1
   }
   { print }
   ' "$CONFIG_FILE" > "$TMP_FILE" && mv "$TMP_FILE" "$CONFIG_FILE"
-  echo "接口添加完成"
-  echo ""
+  echo "网关添加完成"
 fi
+echo ""
+echo ""
 
 # 添加CN_IP别名
 log "$YELLOW" "添加CN_IP别名..."
@@ -116,7 +147,7 @@ if grep -q "<content>https://ispip.clang.cn/all_cn.txt</content>" "$CONFIG_FILE"
 else
   awk '
   BEGIN { inserted = 0 }
-  /<aliases\/>/ && inserted == 0 {
+  /<aliases\/>/ {
     print "        <aliases>"
     print "          <alias uuid=\"315eb669-41ef-48c2-90bb-d5c9d99a00eb\">"
     print "            <enabled>1</enabled>"
@@ -160,13 +191,13 @@ else
   { print }
   ' "$CONFIG_FILE" > "$TMP_FILE" && mv "$TMP_FILE" "$CONFIG_FILE"
     echo "别名添加完成"
-    echo "" 
 fi
+echo ""
 
 # 添加防火墙规则（非中国IP走透明网关TUN_GW）
 log "$YELLOW" "添加防火墙规则..."
 sleep 1
-if grep -q "<gateway>TUN_GW</gateway>" "$CONFIG_FILE"; then
+if grep -q "<address>CN_IP</address>" "$CONFIG_FILE"; then
   echo "存在同名规则，忽略"
   echo ""
 else
@@ -191,112 +222,33 @@ else
     print "        <not>1</not>"
     print "      </destination>"
     print "    </rule>"
+    print "    <rule uuid=\"1c80d0f2-34a9-4303-a598-93c547d4edaa\">"
+    print "      <type>pass</type>"
+    print "      <interface>opt10</interface>"
+    print "      <ipprotocol>inet</ipprotocol>"
+    print "      <statetype>keep state</statetype>"
+    print "      <direction>in</direction>"
+    print "      <quick>1</quick>"
+    print "      <source>"
+    print "       <any>1</any>"
+    print "      </source>"
+    print "      <destination>"
+    print "        <any>1</any>"
+    print "      </destination>"
+    print "    </rule>"
     next
   }
   { print }
   ' "$CONFIG_FILE" > "$TMP_FILE" && mv "$TMP_FILE" "$CONFIG_FILE"
   echo "规则添加完成"
+fi
   echo ""
-fi
-
-# 更改Unbound端口为 5355
-sleep 1
-log "$YELLOW" "更改Unbound端口..."
-
-PORT_OK=$(awk '
-BEGIN {
-  in_unbound = 0
-  in_general = 0
-}
-/<unboundplus[^>]*>/ { in_unbound = 1 }
-/<\/unboundplus>/ { in_unbound = 0 }
-{
-  if (in_unbound && /<general>/) {
-    in_general = 1
-  }
-  if (in_unbound && /<\/general>/) {
-    in_general = 0
-  }
-  if (in_unbound && in_general && /<port>5355<\/port>/) {
-    print "yes"
-    exit
-  }
-}
-' "$CONFIG_FILE")
-
-if [ "$PORT_OK" = "yes" ]; then
-  echo "端口已经为5355，跳过"
-else
-  awk '
-  BEGIN {
-    in_unbound = 0
-    in_general = 0
-    port_handled = 0
-  }
-  {
-    if ($0 ~ /<unboundplus[^>]*>/) {
-      in_unbound = 1
-    }
-    if ($0 ~ /<\/unboundplus>/) {
-      in_unbound = 0
-    }
-
-    if (in_unbound && $0 ~ /<general>/) {
-      in_general = 1
-      print
-      next
-    }
-
-    if (in_unbound && in_general && $0 ~ /<\/general>/) {
-      if (port_handled == 0) {
-        print "        <port>5355</port>"
-        port_handled = 1
-      }
-      in_general = 0
-      print
-      next
-    }
-
-    if (in_unbound && in_general && $0 ~ /<port>.*<\/port>/ && port_handled == 0) {
-      sub(/<port>.*<\/port>/, "<port>5355</port>")
-      port_handled = 1
-      print
-      next
-    }
-
-    print
-  }
-  ' "$CONFIG_FILE" > "$TMP_FILE"
-
-  if [ -s "$TMP_FILE" ]; then
-    mv "$TMP_FILE" "$CONFIG_FILE"
-    echo "端口已设置为5355"
-  else
-    log "$RED" "修改失败，请检查配置文件"
-  fi
-fi
-echo ""
-
-# 重启服务Unbound
-log "$YELLOW" "重启Unbound服务..."
-/usr/local/etc/rc.d/unbound restart > /dev/null 2>&1
-echo ""
-
-# 重新载入防火墙规则
-log "$YELLOW" "重新载入防火墙规则..."
-configctl filter reload > /dev/null 2>&1
-echo ""
-
-# 重新载入configd
-log "$YELLOW" "重新载入configd..."
-service configd restart > /dev/null 2>&1
-echo ""
 
 # 重启所有服务
-# log "$YELLOW" "应用所有更改，请稍等..."
-# /usr/local/etc/rc.reload_all >/dev/null 2>&1
-# echo "所有服务已重新加载！"
-# echo ""
+log "$YELLOW" "重启所有服务，请稍等..."
+/usr/local/etc/rc.reload_all >/dev/null 2>&1
+echo "所有服务已重新加载！"
+echo ""
 
 # 完成提示
 log "$GREEN" "安装完毕，请刷新浏览器，导航到VPN > Sing-Box 菜单进行配置。"
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e ''
-echo -e "\033[32m========Sing-box for OPNsense 代理全家桶一键卸载脚本=========\033[0m"
+echo -e "\033[32m========Sing-box for OPNsense 一键卸载脚本=========\033[0m"
 echo -e ''
 
 # 定义颜色变量
@@ -21,41 +21,33 @@ log() {
 log "$YELLOW" "删除代理程序和配置，请稍等..."
 # 删除配置
 rm -rf /usr/local/etc/sing-box
-rm -rf /usr/local/etc/tun2socks
 
 # 删除rc.d
 rm -f /usr/local/etc/rc.d/singbox
-rm -f /usr/local/etc/rc.d/tun2socks
 
 # 删除rc.conf
-rm -f /etc/rc.conf.d/tun2socks
 rm -f /etc/rc.conf.d/singbox
+
 # 删除action
 rm -f /usr/local/opnsense/service/conf/actions.d/actions_sing-box.conf
-rm -f /usr/local/opnsense/service/conf/actions.d/actions_tun2socks.conf
+
+# 删除inc
+rm -f /usr/local/etc/inc/plugins.inc.d/singbox.inc
+
+# 删除php
+rm -f /usr/local/www/services_sing_box.php
+rm -f /usr/local/www/status_sing_box_logs.php
+rm -f /usr/local/www/status_sing_box.php
+
+
+# 删除程序
+rm -f /usr/local/bin/sing-box
+echo ""
 
 # 删除菜单和缓存
 rm -rf /usr/local/opnsense/mvc/app/models/OPNsense/singbox
 rm -f /tmp/opnsense_menu_cache.xml
 rm -f /tmp/opnsense_acl_cache.json
-
-# 删除inc
-rm -f /usr/local/etc/inc/plugins.inc.d/singbox.inc
-rm -f /usr/local/etc/inc/plugins.inc.d/tun2socks.inc
-
-# 删除php
-rm -f /usr/local/www/services_sing_box.php
-rm -f /usr/local/www/services_tun2socks.php
-rm -f /usr/local/www/status_sing_box_logs.php
-rm -f /usr/local/www/status_sing_box.php
-rm -f /usr/local/www/status_tun2socks_logs.php
-rm -f /usr/local/www/status_tun2socks.php
-rm -f /usr/local/www/sub.php
-
-# 删除程序
-rm -f /usr/local/bin/sing-box
-rm -f /usr/local/bin/tun2socks
-echo ""
 
 # 重启所有服务
 log "$YELLOW" "重新应用所有更改，请稍等..."
@@ -64,5 +56,5 @@ service configd restart > /dev/null 2>&1
 echo ""
 
 # 完成提示
-log "$GREEN" "卸载完成，请手动删除TUN接口、别名和浮动防火墙分流规则，并将Unbound DNS端口更改为53。"
+log "$GREEN" "卸载完成，请手动删除TUN接口、别名和浮动防火墙分流规则。"
 echo ""
